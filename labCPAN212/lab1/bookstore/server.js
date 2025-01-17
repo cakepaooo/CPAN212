e7089e6332dd11c7de2cc0913ca78ff0d1363dad
ducnguyen <nguyennguyen2219@gmail.com>
2025-01-16 21:30:15 -0500

    lab1

--- labCPAN212/lab1/bookstore/server.js
+++ labCPAN212/lab1/bookstore/server.js
@@ -1,13 +1,47 @@
-const express = require("express");
+const http = require("http");
+const fs = require("fs");
 const path = require("path");
 
-const app = express();
 const PORT = 3000;
+const PAGES_DIR = path.join(__dirname, "pages");
 
-// Serve static files from the "pages" folder
-app.use(express.static("pages"));
+// Function to serve HTML files
+const serveFile = (filePath, res) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 - Page Not Found</h1>");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+};
+
+// Create the server
+const server = http.createServer((req, res) => {
+  let filePath;
+
+  switch (req.url) {
+    case "/":
+      filePath = path.join(PAGES_DIR, "index.html");
+      break;
+    case "/about":
+      filePath = path.join(PAGES_DIR, "about.html");
+      break;
+    case "/contact":
+      filePath = path.join(PAGES_DIR, "contact.html");
+      break;
+    default:
+      filePath = path.join(PAGES_DIR, "404.html");
+      break;
+  }
+
+  serveFile(filePath, res);
+});
 
 // Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
